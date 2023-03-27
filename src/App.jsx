4bf0637aa248e6517d31f716706f084a483f255d
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-03-27 08:53:20 -0300

    :fix: correção exemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useReducer, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import CardAdicionar from './components/CardAdicionar'
 import ListItem from './components/ListItem'
 
 function App() {
 
+  // estado base
   const [listaDeTarefas, setListaDeTarefas] = useState([
     {id: 1, texto: "Tarefa 1", finalizado: false}
   ])
-
+  
   function adicionarTarefa(tarefa){
     /* 
       1 [x] receber o texto da tarefa
@@ -19,16 +20,12 @@ function App() {
     */
     const novaTarefa = {id: listaDeTarefas.length + 1, texto: tarefa, finalizado: false}
 
-    // sempre que forem copiar um array ou objeto use o spread
-    const novaListaGeral = [...listaDeTarefas]
-    novaListaGeral.push(novaTarefa)
-
-    setListaDeTarefas(novaListaGeral)
+    setListaDeTarefas([...listaDeTarefas, novaTarefa])
   }
 
   function removerTarefa(tarefa){
 
-    // filtra a tarefa selecionada e assim remove ela
+    // filtra a tarefa selecionada e assim remove a tarefa
     const novaLista = listaDeTarefas.filter(item => item.id != tarefa.id)
 
     setListaDeTarefas(novaLista)
